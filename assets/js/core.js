bae689ac54ffffb91f71094eabde952dbed6831d
Jeethesh Karkera <karkerajeethesh2@gmail.com>
2015-10-28 16:57:26 +0530

    assests

---
+++ assets/js/core.js
@@ -0,0 +1,28 @@
+$(document).ready(function() { 
+		/*place jQuery actions here*/ 
+		var link = "http://localhost/home/index.php/"; // Url to your application (including index.php/)
+	
+		$("ul.products form").submit(function() {
+			// Get the product ID and the quantity 
+			var id = $(this).find('input[name=product_id]').val();
+			var qty = $(this).find('input[name=quantity]').val();
+			
+		 	$.post(link + "home/add_cart_item", { product_id: id, quantity: qty, ajax: '1' },
+  				function(data){	
+ 		 			
+ 		 			if(data == 'true'){
+ 		 		
+    					$.get(link + "home/show_cart", function(cart){ // Get the contents of the url cart/show_cart
+  							$("#cart_content").html(cart); // Replace the information in the div #cart_content with the retrieved data
+						}); 		 
+										
+ 		 			}else{
+ 		 				alert("Product does not exist");
+ 		 			}
+ 			 });
+ 			 
+			
+		});
+	
+	});
+
